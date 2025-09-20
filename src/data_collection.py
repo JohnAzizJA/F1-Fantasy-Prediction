c23bd816620ca4a43571f932ace676a19e24eefb
John <johnazizja10@gmail.com>
2025-09-20 14:00:22 +0300

    add functions to fetch drivers, constructors, and race events data

--- src/data_collection.py
+++ src/data_collection.py
@@ -8,6 +8,67 @@ f1.Cache.enable_cache('fastf1cache')
 
 base_url = "https://api.jolpi.ca/ergast/f1"
 
+def get_drivers(season):
+    url = f"{base_url}/{season}/drivers.json"
+    response = requests.get(url)
+    data = response.json()
+    
+    drivers = data['MRData']['DriverTable']['Drivers']
+    
+    rows = []
+    for driver in drivers:
+        rows.append({
+            "first_name": driver["givenName"],
+            "last_name": driver["familyName"],
+            "driver": driver["code"],
+            "car_number": driver["permanentNumber"],
+            "nationality": driver["nationality"]
+        })
+        
+    return pd.DataFrame(rows)
+
+def get_constructors(season):
+    url = f"{base_url}/{season}/constructors.json"
+    response = requests.get(url)
+    data = response.json()
+    
+    constructors = data['MRData']['ConstructorTable']['Constructors']
+    
+    rows = []
+    for constructor in constructors:
+        rows.append({
+            "constructor": constructor["name"],
+            "nationality": constructor["nationality"]
+        })
+        
+    return pd.DataFrame(rows)
+
+def get_schedule_info(season):
+    try:
+        schedule = f1.get_event_schedule(season)
+        rows = []
+        
+        for _, event in schedule.iterrows():
+                session = f1.get_session(season, event['EventName'], 'R')
+                session.load()
+                
+                rows.append({
+                    "season": season,
+                    "round": event['RoundNumber'],
+                    "event": event['EventName'],
+                    "country": event['Country'],
+                    "location": event['Location'],
+                    "date": event['EventDate'],
+                    "format": event['EventFormat'],
+                    "race_laps": session.total_laps,
+                })
+                
+        return pd.DataFrame(rows)
+    
+    except Exception as e:
+        print(f"Skipping schedule info {season}: {e}")
+        return None
+
 def get_session_results(season, event, session_type):
     try:
         session = f1.get_session(season, event, session_type)
@@ -90,7 +151,7 @@ def get_laptimes(season, event, session_type):
         session.load()
         df = session.laps.copy()
         
-        df = df[['Driver', 'DriverNumber', 'Team', 'LapNumber', 'LapTime', 'Position', 'Sector1Time', 'Sector2Time', 'Sector3Time', 'Stint']]
+        df = df[['Driver', 'DriverNumber', 'Team', 'LapNumber', 'LapTime', 'Position', 'Sector1Time', 'Sector2Time', 'Sector3Time', 'Stint', 'Compound']]
         df.rename(columns={
             'Driver': 'driver',
             'DriverNumber': 'car_number',
@@ -101,7 +162,8 @@ def get_laptimes(season, event, session_type):
             'Sector1Time': 'sector1',
             'Sector2Time': 'sector2',
             'Sector3Time': 'sector3',
-            'Stint': 'stint'
+            'Stint': 'stint',
+            'Compound': 'tyre_compound'
         }, inplace=True)
         
         df['season'] = season
@@ -165,3 +227,28 @@ def get_weather_data(season, event, session_type):
         print(f"Skipping weather {season} {event} {session_type}: {e}")
         return None
     
+def get_race_events(season, event):
+    try:
+        session = f1.get_session(season, event, 'R')
+        session.load()
+        
+        df = session.race_control_messages.copy()
+        
+        df.rename(columns = {
+            'Time': 'time',
+            'Category': 'category',
+            'Message': 'message',
+            'Flag': 'flag',
+            'Scope': 'scope',
+            'Lap': 'lap'
+        }, inplace=True)
+        
+        df['season'] = season
+        df['event'] = event
+        df['session_type'] = 'R'
+
+        return df
+    
+    except Exception as e:
+        print(f"Skipping race events {season} {event}: {e}")
+        return None
